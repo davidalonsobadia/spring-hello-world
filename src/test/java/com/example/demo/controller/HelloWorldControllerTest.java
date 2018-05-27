455629eeb2db11bf4d30f6cb05c118e54976ecd6
David Alonso <david.alonso@ocado.com>
2018-05-27 02:30:52 +0200

    Initial commit

---
+++ src/test/java/com/example/demo/controller/HelloWorldControllerTest.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloWorldControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void should_return_hello_world_when_endpoint_is_called() throws Exception {
+
+        this.mockMvc
+                .perform(get("/hello-world"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(is("Hello World!")));
+    }
+}
